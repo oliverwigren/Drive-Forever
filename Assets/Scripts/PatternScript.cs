b3e385ce3e8c42f76e94ffb893973005230b0f12
Oliver Wigren <oliver.wigren@hotmail.com>
2023-12-27 23:51:25 +0100

    removed speed-change from patterns

--- Assets/Scripts/PatternScript.cs
+++ Assets/Scripts/PatternScript.cs
@@ -12,30 +12,30 @@ public class PatternScript : MonoBehaviour
 
     //bool editor;
 
-    public float speed = 10;
+    private const float speed = 7;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         Invoke("DestroyObject", 9);
 
-#if UNITY_EDITOR
-{
-            koefficient = 1;
-}
-#elif UNITY_STANDALONE_OSX
-{
-koefficient = 0.5f;
-}
-#elif UNITY_ANDROID
-{
-koefficient = 1;
-}
-#elif UNITY_IOS
-{
-koefficient = 0.5f;
-}
-#endif
+//#if UNITY_EDITOR
+//{
+//            koefficient = 1;
+//}
+//#elif UNITY_STANDALONE_OSX
+//{
+//koefficient = 0.5f;
+//}
+//#elif UNITY_ANDROID
+//{
+//koefficient = 1;
+//}
+//#elif UNITY_IOS
+//{
+//koefficient = 0.5f;
+//}
+//#endif
     }
 
     // Update is called once per frame
@@ -43,7 +43,7 @@ koefficient = 0.5f;
     {
         //rb.MovePosition(transform.position + endPosition.transform.position * Time.deltaTime * koefficient);
 
-        transform.Translate(Vector2.down * speed * Time.deltaTime * koefficient);
+        transform.Translate(Vector2.down * speed * Time.deltaTime /** koefficient*/);
     }
 
     void DestroyObject()
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -19,7 +19,7 @@ public class Spawner : MonoBehaviour
     public float minTime;
     public float minHeight;
     public float maxHeight;
-    public float minSpawn;
+    //public float minSpawn;
 
     public GameManager gameManager;
 
@@ -42,7 +42,7 @@ public class Spawner : MonoBehaviour
         }
         else
         {
-            TimeBtwSpawns -= Time.deltaTime;
+            TimeBtwSpawns -= Time.deltaTime; //* decrease;
         }
     }
 
@@ -96,21 +96,21 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        if(startTimeBtwSpawns > 0.65f*k)
+        if(startTimeBtwSpawns > 0.8f*k)
         {
             Debug.Log("Easy");
             GameObject obstacle = Instantiate(easyPatterns[Random.Range(0, easyPatterns.Length)], new Vector3(transform.position.x, transform.position.y, -1), Quaternion.identity);
             obstacle.transform.position += Vector3.left * Random.Range(minHeight, maxHeight);
             gameManager.GetComponent<GameManager>().AddScore(50);
         }
-        if (startTimeBtwSpawns <= 0.65f*k && startTimeBtwSpawns > 0.50f*k)
+        if (startTimeBtwSpawns <= 0.8f*k && startTimeBtwSpawns > 0.7f*k)
         {
             Debug.Log("Medium");
             GameObject obstacle = Instantiate(mediumPatterns[Random.Range(0, mediumPatterns.Length)], new Vector3(transform.position.x, transform.position.y, -1), Quaternion.identity);
             obstacle.transform.position += Vector3.left * Random.Range(minHeight, maxHeight);
             gameManager.GetComponent<GameManager>().AddScore(100);
         }
-        if (startTimeBtwSpawns <= 0.50f *k/*&& startTimeBtwSpawns > 0.6f*/)
+        if (startTimeBtwSpawns <= 0.7f *k/*&& startTimeBtwSpawns > 0.6f*/)
         {
             Debug.Log("Hard");
             GameObject obstacle = Instantiate(hardPatterns[Random.Range(0, hardPatterns.Length)], new Vector3(transform.position.x, transform.position.y, -1), Quaternion.identity);
